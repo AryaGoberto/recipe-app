b4d30077937fe98e023bc135cd620314fce54b6a
AryaGoberto <arguna2019@gmail.com>
2025-12-19 09:56:15 +0800

    flutter setup

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(RecipeApp());
+}
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
